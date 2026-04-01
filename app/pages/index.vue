d2b44a7d067d5fe6f90b7c283413bc6c865a79ec
Pavel5284 <pavel5284@mail.ru>
2026-04-02 00:18:24 +0200

    feat: drag and drop

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,9 @@
 import {useKanbanQuery} from '@/components/kanban/useKanbanQuery'
 import type {ICard, IColumn} from '~/components/kanban/kanban.types'
 import dayjs from 'dayjs'
+import type {EnumStatus} from "~/types/deals.types"
+import { COLLECTION_DEALS, useDbId} from "~/app.constants"
+import {getDb} from "~/utils/appwite"
 
 useSeoMeta({
   title: "Home | CRM System"
@@ -15,7 +18,53 @@ const {data, isLoading, refetch} = useKanbanQuery()
 onMounted(() => {
   refetch()
 })
-console.log(data, "data")
+
+type TypeMutationVariables = {
+  docId: string
+  status?: EnumStatus
+}
+const DB_ID = useDbId()
+const {mutate} = useMutation({
+  mutationKey: ['move card'],
+  mutationFn: ({docId, status}: TypeMutationVariables) =>{
+  const DB = getDb()
+  return DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+    status
+  })},
+  onSuccess: () => {
+    refetch()
+  }
+})
+
+function onDragStart(event: DragEvent, card: ICard, column: IColumn) {
+  if (event.dataTransfer) {
+    event.dataTransfer.effectAllowed = 'move'
+    event.dataTransfer.setData('text/plain', JSON.stringify({ card, columnId: column.id }))
+  }
+  dragCardRef.value = card
+  sourceColumnRef.value = column
+}
+
+function onDragOver(event: DragEvent) {
+  event.preventDefault()
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move'
+  }
+}
+
+function onDrop(event: DragEvent, targetColumn: IColumn) {
+  event.preventDefault()
+  if (dragCardRef.value && sourceColumnRef.value) {
+    mutate({docId: dragCardRef.value.id, status: targetColumn.id})
+    dragCardRef.value = null
+    sourceColumnRef.value = null
+  }
+}
+
+function onCardDragStart(event: DragEvent, card: ICard, column: IColumn) {
+  onDragStart(event, card, column)
+}
+
 </script>
 
 <template>
@@ -25,18 +74,21 @@ console.log(data, "data")
   <div v-if="isLoading">Loading...</div>
   <div v-else>
     <div class="grid grid-cols-5 gap-16">
-      <div v-for="(column, index) in data"
+      <div v-for="column in data"
            :key="column.id"
+           @dragover="onDragOver"
+           @drop="onDrop($event, column)"
       >
         <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
           {{ column.name }}
         </div>
         <KanbanCreateDeal :refetch="refetch" :status="column.id"/>
-        <UiCard v-for="card in column.items"
+<UiCard v-for="card in column.items"
                 :key="card.id"
                 class="mb-3"
                 draggable="true"
-        >
+                @dragstart="onCardDragStart($event, card, column)"
+>
           <UiCardHeader role="button">
             <UiCardTitle>
               {{ card.name }}
